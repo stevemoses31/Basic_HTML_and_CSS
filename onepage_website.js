d3fb92a3fa397ecbbd15bfbd1512975204782c2d
stevemoses31 <134295998+stevemoses31@users.noreply.github.com>
2023-09-04 14:40:32 -0700

    a

--- onepage_website.js
+++ onepage_website.js
@@ -21,14 +21,6 @@ function openModal() {
     showSlides(slideIndex = n);
   }
 
-
-  function openModal() {
-    document.getElementById("myModal").style.display = "block";
-  }
-  function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-  }
-
   var slideIndex = 1;
   showSlides(slideIndex);
 
